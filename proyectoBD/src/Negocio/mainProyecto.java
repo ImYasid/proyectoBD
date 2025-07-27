fe7c3e7267918f8a0dd49b0ebf21afd81a245491
Yasid Jimenez <138026573+ImYasid@users.noreply.github.com>
2025-07-27 15:24:18 -0500

    :sparkles: new actions

---
+++ proyectoBD/src/Negocio/mainProyecto.java
@@ -0,0 +1,12 @@
+package Negocio;
+import Vista.JFlogin;
+
+public class mainProyecto {
+
+    public static void main(String[] args) {
+       JFlogin inicio = new JFlogin();
+       inicio.setVisible(true);
+        
+    }
+    
+}
